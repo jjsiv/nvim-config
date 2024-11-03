f4acb74beaa2544e917104941faf66903325391f
Jedrzej Kotkowski <jedrzejk143@gmail.com>
2024-11-03 18:34:37 +0100

    mason up

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,4 +1,46 @@
 return {
     "williamboman/mason.nvim",
-    opts = {},
+    config = function()
+        require('mason').setup()
+        local registry = require('mason-registry')
+
+        registry:on(
+            "package:handle",
+            vim.schedule_wrap(function(pkg, handle)
+                print(string.format("Installing %s", pkg.name))
+            end)
+        )
+
+        registry:on(
+            "package:install:success",
+            vim.schedule_wrap(function(pkg, handle)
+                print(string.format("Successfully installed %s", pkg.name))
+            end)
+        )
+
+        registry:on(
+            "package:install:failed",
+            vim.schedule_wrap(function(pkg, handle)
+                print(string.format("Failed to install %s", pkg.name))
+            end)
+        )
+
+        ---@param packages table<string>
+        local function install_packages(packages)
+            for _, package in ipairs(packages) do
+                local installed = registry.is_installed(package)
+                if not installed then
+                    if registry.has_package(package) then
+                        registry.get_package(package):install()
+                    end
+                end
+            end
+        end
+
+        install_packages({
+            'prettier',
+            'ansible-lint'
+        })
+
+    end,
 }
