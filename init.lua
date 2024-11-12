f770d437069ff65b9b1149c0eb3eacf8f737d2f4
Jedrzej Kotkowski <jedrzejk143@gmail.com>
2024-11-12 19:14:32 +0100

    some lsp improvements

--- init.lua
+++ init.lua
@@ -23,10 +23,10 @@ local function set_colorscheme(colorscheme)
   end
   -- set gold color to line numbers, should be visible on most schemes
   -- set at the end to overwrite any colorscheme config
-  vim.api.nvim_set_hl(0, "LineNr", { bold = true, bg = "#2A2A37" })
+  --vim.api.nvim_set_hl(0, "LineNr", { bold = true, bg = "#2A2A37" })
 end
 
-set_colorscheme("kanagawa")
+set_colorscheme("kanagawa-dragon")
 
 -- :h option-list for all options
 -- :h <option> for doc about option
@@ -44,22 +44,9 @@ set_opts({
   termguicolors = true,
   scrolloff = 10,
   splitright = true,
-  completeopt = { 'menu', 'menuone', 'noselect' }
+  completeopt = { 'menu', 'menuone' }
 })
 
--- keymappings
--- remap tab to ctrl+n in popupmenu
-vim.keymap.set('i', '<Tab>', function()
-  return vim.fn.pumvisible() == 1 and "<C-n>" or "<Tab>"
-end)
--- remap shift+tab to ctrl+p in popupmenu
-vim.keymap.set('i', '<S-Tab>', function()
-  return vim.fn.pumvisible() == 1 and "<C-p>" or "<BS>"
-end)
--- do ctrl+y when tab is pressed in a popumenu
-vim.keymap.set('i', '<Tab>', function()
-  if vim.fn.pumvisible() == 1 then return "<C-y>" else return "<Tab>" end
-end, { expr = true })
 -- next buffer ctrl+n
 vim.keymap.set('n', "<C-n>", "<cmd>bnext<CR>", {})
 vim.keymap.set('n', "<C-p>", "<cmd>bprev<CR>", {})
--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -10,8 +10,28 @@ return {
       auto_start = "shut-up",
       keymap = {
         recommended = false,
-        pre_select = false
+        pre_select = true
+      },
+      limits = {
+        completion_auto_timeout = 0,
       }
     }
+    -- mappings for coq_nvim
+    -- remap tab to ctrl+n in popupmenu
+    vim.keymap.set('i', '<Tab>', function()
+      return vim.fn.pumvisible() == 1 and "<C-n>" or "<Tab>"
+    end)
+    -- remap shift+tab to ctrl+p in popupmenu
+    vim.keymap.set('i', '<S-Tab>', function()
+      return vim.fn.pumvisible() == 1 and "<C-p>" or "<BS>"
+    end)
+    -- do ctrl+y when tab is pressed in a popumenu
+    vim.keymap.set('i', '<Tab>', function()
+      if vim.fn.pumvisible() == 1 then return "<C-y>" else return "<Tab>" end
+    end, { expr = true })
+
+    -- other LSP related keybindings
+    vim.keymap.set('n', '<Leader>rn', function() vim.lsp.buf.rename() end, {})
+    vim.keymap.set('n', '<Leader>d', function() vim.diagnostic.open_float() end, {})
   end,
 }
