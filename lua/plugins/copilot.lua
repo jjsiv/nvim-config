512bcffb5774c64f384ba3fe8733acb3b1c260ee
Jedrzej Kotkowski <jedrzejk143@gmail.com>
2024-12-20 00:03:39 +0100

    more copilot crap

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -5,8 +5,12 @@ return {
   event = "InsertEnter",
   config = function()
     require("copilot").setup({
-      panel = { enabled = false },
-      suggestion = { enabled = false },
+      panel = { enabled = true, auto_refresh = true },
+      suggestion = {
+        enabled = false,
+        auto_trigger = true,
+        hide_during_completion = false,
+      },
     })
 
     require("copilot_cmp").setup()
