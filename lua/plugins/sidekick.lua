456c1de26eaf2c9e51f047a23e23391771d3db10
Jedrzej Kotkowski <jedrzejk143@gmail.com>
2026-06-11 17:55:17 +0200

    sidekick again

---
+++ lua/plugins/sidekick.lua
@@ -0,0 +1,73 @@
+return {
+  "folke/sidekick.nvim",
+  lazy = false,
+  config = function()
+    require("sidekick").setup({
+      nes = {
+        enabled = false,
+      },
+      cli = {
+        mux = {
+          enabled = true,
+          backend = "tmux",
+          create = "terminal",
+        },
+      },
+    })
+  end,
+  keys = {
+    {
+      "<leader>cf",
+      function()
+        require("sidekick.cli").focus()
+      end,
+      desc = "Sidekick Focus",
+      mode = { "n", "t", "i", "x" },
+    },
+    {
+      "<leader>cc",
+      function()
+        require("sidekick.cli").toggle({ name = "claude", focus = false })
+      end,
+      desc = "Toggle Claude Code",
+    },
+    {
+      "<leader>cd",
+      function()
+        require("sidekick.cli").hide()
+      end,
+      desc = "Hide CLI",
+    },
+    {
+      "<leader>at",
+      function()
+        require("sidekick.cli").send({ msg = "{this}" })
+      end,
+      mode = { "x", "n" },
+      desc = "Send This",
+    },
+    {
+      "<leader>af",
+      function()
+        require("sidekick.cli").send({ msg = "{file}" })
+      end,
+      desc = "Send File",
+    },
+    {
+      "<leader>av",
+      function()
+        require("sidekick.cli").send({ msg = "{selection}" })
+      end,
+      mode = { "x" },
+      desc = "Send Visual Selection",
+    },
+    {
+      "<leader>ap",
+      function()
+        require("sidekick.cli").prompt()
+      end,
+      mode = { "n", "x" },
+      desc = "Sidekick Select Prompt",
+    },
+  },
+}
